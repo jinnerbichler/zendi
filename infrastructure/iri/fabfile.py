76a171cca678ce34c82e2bd1a02ca208cb0383bf
Johannes Innerbichler <johannes.innerbichler@salzburgresearch.at>
2017-11-07 06:21:25 +0100

    IRI deployment improvements

--- infrastructure/iri/fabfile.py
+++ infrastructure/iri/fabfile.py
@@ -1,12 +1,24 @@
 from fabric.api import run, env, task, put, cd, local
 
+env.use_ssh_config = True
+env.hosts = ['iota_mail_1']
+
 
 @task(default=True)
 def deploy():
-    local('docker-compose pull')
-    local('docker-compose --project-name iri up -d --force-recreate')
+    run('mkdir -p /srv/iri/')
+    with cd('/srv/iri'):
+        put('.', '.')
+        run('docker-compose pull')
+        run('docker-compose --project-name iri up -d --force-recreate')
 
 
 @task
 def logs():
-    local('docker-compose logs -f')
+    with cd('/srv/iri'):
+        run('docker-compose logs -f')
+
+
+@task
+def run_local():
+    local('docker-compose --project-name iri up -d --force-recreate')
